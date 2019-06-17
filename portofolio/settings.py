8a114726c318eab9f8b09de92da9da189ca023c4
MujeebUrRehman58 <mrehman.bscs15seecs@seecs.edu.pk>
2019-06-17 16:54:51 +0500

    job app up ad running

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portofolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portofoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'QWEasd&786',
+        'HOST': '127.0.0.1',
+        'PORT': '5432', 
     }
 }
 
