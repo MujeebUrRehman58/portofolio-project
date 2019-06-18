c1819c0e35f06fdb96dfca61c1b59edbf07dc6ff
MujeebUrRehman58 <mrehman.bscs15seecs@seecs.edu.pk>
2019-06-18 14:57:37 +0500

    blog polish

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portofoliodb',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'QWEasd&786',
         'HOST': '127.0.0.1',
         'PORT': '5432', 
     }
